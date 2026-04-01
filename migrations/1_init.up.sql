e3f1aff79c03cf393129a0205707685cb3cbba71
SMRiST <s.m.r.00800ist@gmail.com>
2026-04-01 10:12:40 +0300

    Added migrations

--- migrations/1_init.up.sql
+++ migrations/1_init.up.sql
@@ -1,5 +1,8 @@
-CREATE TABLE books (
+CREATE TABLE IF NOT EXISTS reservations (
     id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
     author TEXT NOT NULL,
-    title TEXT NOT NULL
+    title TEXT NOT NULL,
+    taken_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    return_at TIMESTAMPTZ
 );
